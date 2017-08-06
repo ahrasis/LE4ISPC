79a30f2add76400c8aaea69b4e5be8a19c45ac2c
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2017-08-06 09:57:27 +0800

    Create le_ispc_pem.sh

---
+++ apache/le_ispc_pem.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:  LE ISPSERVER.PEM AUTO UPDATER
+# Required-Start:  $local_fs $network
+# Required-Stop:  $local_fs
+# Default-Start:  2 3 4 5
+# Default-Stop:  0 1 6
+# Short-Description:  LE ISPSERVER.PEM AUTO UPDATER
+# Description:  Update ispserver.pem automatically after ISPC LE SSL certs are renewed.
+### END INIT INFO
+cd /usr/local/ispconfig/interface/ssl/
+mv ispserver.pem ispserver.pem-$(date +"%y%m%d%H%M%S").bak
+cat ispserver.{key,crt} > ispserver.pem
+chmod 600 ispserver.pem
+chmod 600 /etc/ssl/private/pure-ftpd.pem
+service pure-ftpd-mysql restart
+service monit restart
+service postfix restart
+service dovecot restart
+service apache2 restart
