ce94f9c9281bb1c16184f031374d1165055713c1
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2018-07-19 15:27:54 +0800

    Update le_ispc_pem.sh
    Add support in securing mysql server

--- nginx/le_ispc_pem.sh
+++ nginx/le_ispc_pem.sh
@@ -13,6 +13,9 @@ mv ispserver.pem ispserver.pem-$(date +"%y%m%d%H%M%S").bak
 cat ispserver.{key,crt} > ispserver.pem
 chmod 600 ispserver.pem
 chmod 600 /etc/ssl/private/pure-ftpd.pem
+scp ispserver.crt /etc/mysql/server-cert.pem
+scp ispserver.key /etc/mysql/server-key.pem
+service mysql restart
 service pure-ftpd-mysql restart
 if [ -d "/etc/monit" ]; then
   service monit restart
@@ -21,10 +24,19 @@ service postfix restart
 service dovecot restart
 service nginx restart
 
+# IMPORTANT NOTES 
+#
 # Enable and modify accordingly to automate to transfer LE SSL certs to other servers 
 # scp -P XX -r /etc/letsencrypt/ root@server2.domain.tld:~/etc/
 # Use -P XX if your ssh use different port then default 22, where XX is assigned ssh port number.
+#
 # You can also use rsync if you have it installed in your system
 # rsync -a -e "ssh -p XX" /etc/letsencrypt/ root@server2.domain.tld:~/etc/letsencrypt/
 # Use "ssh -p XX" if your ssh use different port then default 22, where XX is assigned ssh port number.
 # Copy and add similar line if you have more server(s).
+#
+# To update a secured remote mysql server automatically try something like these:
+# scp ispserver.crt root@192.168.0.3:/etc/mysql/server-cert.pem
+# scp ispserver.key root@192.168.0.3:/etc/mysql/server-key.pem
+# ssh root@192.168.0.3 'service mysql restart'
+# Change root@192.168.0.3 accordingly to ssh access.
