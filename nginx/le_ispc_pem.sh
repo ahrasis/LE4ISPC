6bc13c7628c515dc350509902a3f0afba9f7852b
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2018-09-18 12:26:15 +0800

    Update le_ispc_pem.sh

--- nginx/le_ispc_pem.sh
+++ nginx/le_ispc_pem.sh
@@ -20,8 +20,6 @@ service mysql restart
 service pure-ftpd-mysql restart
 if [ -d "/etc/monit" ]; then service monit restart; fi
 if [ -d "/etc/postfix" ]; then service postfix restart; service dovecot restart; fi
-service postfix restart
-service dovecot restart
 service nginx restart
 
 # IMPORTANT NOTES 
