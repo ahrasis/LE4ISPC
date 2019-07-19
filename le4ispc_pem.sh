afd570d706de79ee15c7fa95e901cd3ad6d7320b
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2019-07-19 09:32:22 +0800

    Update le4ispc_pem.sh
    Fixed dovecot not restarted. Shorten and removed unnecessary code including mysql.

--- le4ispc_pem.sh
+++ le4ispc_pem.sh
@@ -8,55 +8,21 @@
 # Short-Description:  LE ISPSERVER.PEM AUTO UPDATER
 # Description:  Update ispserver.pem automatically after ISPC LE SSL certs are renewed.
 ### END INIT INFO
-
 # Enable set -e to cause script to exit on error
+# Recreate pem file if letsencrypt ssl certs exist
+# then restart all available service(s)
 set -e
-
-# Check the letsencrypt ssl certs path
-# then recreate pem file and finally  
-# restart all relevant service(s)
-lelive=/etc/letsencrypt/live/$(hostname -f)
-if [ -d "$lelive" ]; then
-	if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-		websvr=nginx
-	else
-		websvr=apache2
-	fi
-	cd /usr/local/ispconfig/interface/ssl
-	ispcbak=ispserver.*.bak
-	ispccrt=ispserver.crt
-	ispckey=ispserver.key
-	ispcpem=ispserver.pem
-	if ls $ispcbak 1> /dev/null 2>&1; then rm $ispcbak; fi
-	if [ -e "$ispcpem" ]; then mv $ispcpem $ispcpem-$(date +"%y%m%d%H%M%S").bak; fi
-	cat $ispckey $ispccrt > $ispcpem
-	chmod 600 $ispcpem
-	if [ $(dpkg-query -W -f='${Status}' $websvr 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-		service $websvr restart
-	fi
-	if [ $(dpkg-query -W -f='${Status}' mysql-server 2>/dev/null | grep -c "ok installed") -eq 1 ] || [ $(dpkg-query -W -f='${Status}' mariadb-server 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-		scp ispserver.crt /etc/mysql/server-cert.pem
-		scp ispserver.key /etc/mysql/server-key.pem
-		service mysql restart
-	fi
-	if [ $(dpkg-query -W -f='${Status}' pure-ftpd-mysql 2>/dev/null | grep -c "ok installed") -eq 1 ] || [ $(dpkg-query -W -f='${Status}' monit 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-		chmod 600 /etc/ssl/private/pure-ftpd.pem
-		if [ $(dpkg-query -W -f='${Status}' pure-ftpd-mysql 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-			service pure-ftpd-mysql restart
-		fi
-		if [ $(dpkg-query -W -f='${Status}' monit 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-			service monit restart
-		fi
-	fi
-	if [ $(dpkg-query -W -f='${Status}' postfix 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-		service postfix restart
-	fi
-	if [ $(dpkg-query -W -f='${Status}' dovecot 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-		service dovecot restart
-	fi
-	if [ $(dpkg-query -W -f='${Status}' $websvr 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
-		service $websvr restart
-	fi
-else
-	echo "Your Lets Encrypt SSL certs path for your ISPConfig server FQDN is missing."
-fi
+lelive=/etc/letsencrypt/live/$(hostname -f); if [ -d "$lelive" ]; then
+  cd /usr/local/ispconfig/interface/ssl; ibak=ispserver.*.bak; ipem=ispserver.pem; icrt=ispserver.crt; ikey=ispserver.key
+  if ls $ibak 1> /dev/null 2>&1; then rm $ibak; fi
+  if [ -e "$ipem" ]; then mv $ipem $ipem-$(date +"%y%m%d%H%M%S").bak; cat $ikey $icrt > $ipem; chmod 600 $ipem; fi
+  pureftpdpem=/etc/ssl/private/pure-ftpd.pem; if [ -e "$pureftpdpem" ]; then chmod 600 $pureftpdpem; fi
+  if [ $(dpkg-query -W -f='${Status}' pure-ftpd-mysql 2>/dev/null | grep -c "ok installed") -eq 1 ]; then service pure-ftpd-mysql restart; fi
+  if [ $(dpkg-query -W -f='${Status}' monit 2>/dev/null | grep -c "ok installed") -eq 1 ]; then service monit restart; fi
+  if [ $(dpkg-query -W -f='${Status}' postfix 2>/dev/null | grep -c "ok installed") -eq 1 ]; then service postfix restart; fi
+  if [ $(dpkg-query -W -f='${Status}' dovecot-imapd 2>/dev/null | grep -c "ok installed") -eq 1 ]; then service dovecot restart; fi
+  if [ $(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed") -eq 1 ]; then service nginx restart; fi
+  if [ $(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c "ok installed") -eq 1 ]; then service apache2 restart; fi
+else echo "Your Lets Encrypt SSL certs path for your ISPConfig server FQDN is missing."; fi
+# Let's Encrypt will not work for mysql, so the related code were removed.
+# The whole code is shorten simplified whenever possile.
