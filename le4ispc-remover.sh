a77d0f317a3b6c29d94d1a6bc458754236689a28
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2020-12-23 17:19:25 +0800

    Create le4ispc-remover.sh

---
+++ le4ispc-remover.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:  CREATE LE SSL FOR ISPC AND OTHER MAIN SERVICES
+# Required-Start:  $local_fs $network
+# Required-Stop:  $local_fs
+# Default-Start:  2 3 4 5
+# Default-Stop:  0 1 6
+# Short-Description:  CREATE LE SSL FOR ISPC AND OTHER MAIN SERVICES
+# Description:  Create LE SSL for ISPC and other main services.
+### END INIT INFO
+
+# Enable set -e to cause script to exit on error
+set -e
+
+le4ispc_pem=/etc/init.d/le4ispc_pem.sh
+if [ -e "$le4ispc_pem" ]; then rm $le4ispc_pem; fi
+
+iroot=/var/spool/incron/root
+if [ -e "$iroot" ] && grep -q "le_ispc_pem.sh" $iroot; then sed -i '/le_ispc_pem.sh/d' $iroot; fi
+if [ -e "$iroot" ] && grep -q "le_ispc_pem.sh" $iroot; then sed -i '/le4ispc_pem.sh/d' $iroot; fi
+chmod 600 $iroot
+service incron restart
